d658695121a45a6126f33052e363214fe558ce5c
hugo <vvhooping@gmail.com>
2025-10-17 14:15:53 +0900

    chore: Prettier 설치 및 설정

---
+++ eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 한 줄의 최대 길이를 100으로 제한
+  printWidth: 100,
+  // 세미콜론을 문장의 끝에 항상 붙임
+  semi: true,
+  // 문자열을 홑따옴표(')로 통일
+  singleQuote: true,
+  // 탭 대신 스페이스를 사용
+  useTabs: false,
+  // 탭의 너비를 2칸으로 설정
+  tabWidth: 2,
+  // 객체 리터럴에서 괄호와 내용 사이에 공백 추가
+  bracketSpacing: true,
+  // 화살표 함수의 매개변수가 하나일 때 괄호 생략
+  arrowParens: 'avoid',
+  // 파일 끝에 개행 문자 추가
+  endOfLine: 'auto',
+  // 후행 쉼표 사용 안 함
+  trailingComma: 'none'
+};
