9f890dac4189addd94df536b3c13ff0a713868ba
hugo <vvhooping@gmail.com>
2025-10-18 14:29:38 +0900

    chore: 경로 별칭 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import vitePluginSvgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), vitePluginSvgr()]
+  plugins: [react(), tailwindcss(), vitePluginSvgr()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  }
 });
