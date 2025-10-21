552b341f45411ca2e496dba03b96b3fe67f3b3a2
hugo <vvhooping@gmail.com>
2025-10-21 22:45:54 +0900

    style: 게시글 제목에 말줄임표 표시 추가

--- src/features/posts/components/PostTableRow.tsx
+++ src/features/posts/components/PostTableRow.tsx
@@ -12,10 +12,10 @@ export default function PostTableRow({ post }: PostTableRowProps) {
   return (
     <tr className="border-b border-slate-300 hover:bg-slate-50 ">
       <td className="p-2 text-left">{no}</td>
-      <td className="p-2 text-left">
+      <td className="p-2 text-left max-w-0">
         <Link to={`/posts/${_id}`} className="flex items-center cursor-pointer hover:font-[500]">
-          {title}
-          <div className="flex items-center gap-0.5 ml-2">
+          <span className="truncate">{title}</span>
+          <div className="flex flex-shrink-0 items-center gap-0.5 ml-2">
             <ChatBubbleLeftIcon className="inline-block w-3 h-3" />
             <span className="text-gray-600 text-xs">{commentCount}</span>
           </div>
